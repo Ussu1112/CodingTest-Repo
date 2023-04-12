d67150cf84713a49e7a97560f7b51b20a66b275c
Song Tae-Hyoung <ussu1112@naver.com>
2023-04-12 12:56:37 +0900

    [Bronze II] Title: OX퀴즈, Time: 232 ms, Memory: 17856 KB -BaekjoonHub

---
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        for(int i = 0; i < T; i++){
+            foo(sc.next());
+        }
+    }
+
+    private static void foo(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        int i = 0;
+        int sum = 0;
+
+        while (i < s.length()){
+            char c = s.charAt(i);
+
+            if (c == 'O') {
+                stack.push(c);
+                sum += stack.size();
+            } else {
+                stack.clear();
+            }
+            i++;
+        }
+        System.out.println(sum);
+    }
+}
