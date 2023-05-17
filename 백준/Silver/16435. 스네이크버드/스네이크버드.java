3236030dab83d110bb4c0ec53a5b7a75bb119604
Song Tae-Hyoung <ussu1112@naver.com>
2023-05-17 14:41:45 +0900

    [Silver V] Title: 스네이크버드, Time: 264 ms, Memory: 20824 KB -BaekjoonHub

---
+++ 백준/Silver/16435. 스네이크버드/스네이크버드.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int L = sc.nextInt();
+        int[] H = new int[N];
+
+        for (int i = 0; i < H.length; i++) {
+            H[i] = sc.nextInt();
+        }
+
+        Arrays.sort(H);
+
+        for (int i = 0; i < H.length; i++) {
+            if (L >= H[i]){
+                L++;
+            } else {
+                break;
+            }
+        }
+        System.out.println(L);
+    }
+}
