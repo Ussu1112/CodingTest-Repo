2dec1b2a70c8eb72a7aa72c228f2508ed2aff1b5
Song Tae-Hyoung <ussu1112@naver.com>
2023-05-26 13:58:14 +0900

    [Silver IV] Title: 카드2, Time: 196 ms, Memory: 53296 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int cardNum = Integer.parseInt(br.readLine());
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 1; i <= cardNum; i++) {
+            queue.offer(i);
+        }
+
+        int temp = 0;
+        while (queue.size() > 1){
+            queue.poll();
+            temp = queue.poll();
+            queue.offer(temp);
+        }
+
+        System.out.println(queue.poll());
+    }
+}
