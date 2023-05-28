ab740d5657bf8b690ea18df7cfab65ef7af6ef31
Song Tae-Hyoung <ussu1112@naver.com>
2023-05-28 21:23:32 +0900

    [Bronze V] Title: 꼬마 정민, Time: 120 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long answer = 0;
+
+        for (int i = 0; i < 3; i++) {
+            answer += Long.parseLong(st.nextToken());
+        }
+
+        System.out.println(answer);
+    }
+}
