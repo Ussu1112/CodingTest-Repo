c55f2889f4dedd4768ce843aabcea0cf41ec9e50
Song Tae-Hyoung <ussu1112@naver.com>
2023-06-08 22:15:30 +0900

    [Bronze V] Title: 아스키 코드, Time: 200 ms, Memory: 17612 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        char letter = sc.next().charAt(0);
+        int result = letter;
+
+        System.out.println(result);
+    }
+}
