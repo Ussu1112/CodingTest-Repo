3ffa006ffe6a00471fc7407c21842993a6390358
Song Tae-Hyoung <ussu1112@naver.com>
2023-10-18 20:33:44 +0900

    [Bronze I] Title: 단어 공부, Time: 528 ms, Memory: 31856 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,28 +1,37 @@
+import java.util.Arrays;
 import java.util.Scanner;
-
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String input1 = sc.nextLine();
+        System.out.println(solution(input1));
+    }
 
-        char[] input = sc.nextLine().toLowerCase().replaceAll(" ", "").toCharArray();
+    private static char solution(String input1) {
+        int[] alphabets = getAlphabetCount(input1);
+        int maxCount = -1;
+        char maxAlphabet = '?';
 
-        int[] counts = new int['z' - 'a' + 1];
 
-        for (int i = 0; i < input.length; i++){
-            counts[input[i]-'a']++;
-        }
-        int max = 0;
-        char result = 63; // ?
-
-        for (int j = 0; j < counts.length; j++){
-            if(counts[j] > max) {
-                max = counts[j];
-                result = (char) (j + 'A');
-            } else if (counts[j] == max) {
-                result = 63;
+        for (int i = 0; i < alphabets.length; i++) {
+            if (alphabets[i] > maxCount){
+                maxCount = alphabets[i];
+                maxAlphabet = (char)('A'+ i);
+            } else if (alphabets[i] == maxCount){
+                maxAlphabet = '?';
             }
         }
-        System.out.println(result);
+        
+        return maxAlphabet;
+    }
+
+    private static int[] getAlphabetCount(String input1) {
+        int[] count = new int[26];
+        input1 = input1.toUpperCase();
+        for (int i = 0; i < input1.length(); i++) {
+            count[input1.charAt(i) - 'A']++;
+        }
+        return count;
     }
 }
